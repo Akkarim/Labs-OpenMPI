52bddcfc6669ba0f64e3a8a41524d0b267a21c2f
Felipe Carmona <luis.carmonacastro@ucr.ac.cr>
2018-11-19 08:46:30 -0600

    Update main3.cpp
    Cambios, no lo toque perro

--- ejercicio3/ejercicio3/main3.cpp
+++ ejercicio3/ejercicio3/main3.cpp
@@ -17,12 +17,12 @@ bool esPrimo(int n);
 int main(int argc, char* argv[]) {
 	int mid; // id de cada proceso
 	int cnt_proc; // cantidad de procesos
-	MPI_Status mpi_status; // para capturar estado al finalizar invocación de funciones MPI
-	double start, stop;
+	MPI_Status mpi_status; // para capturar estado al finalizar invocaciÃ³n de funciones MPI
+	double local_start, local_finish, local_elapsed, elapsed;
 						   /* Arrancar ambiente MPI */
 	MPI_Init(&argc, &argv);             		/* Arranca ambiente MPI */
 	MPI_Comm_rank(MPI_COMM_WORLD, &mid); 		/* El comunicador le da valor a id (rank del proceso) */
-	MPI_Comm_size(MPI_COMM_WORLD, &cnt_proc);  /* El comunicador le da valor a p (número de procesos) */
+	MPI_Comm_size(MPI_COMM_WORLD, &cnt_proc);  /* El comunicador le da valor a p (nÃºmero de procesos) */
 
 #  ifdef DEBUG 
 	if (mid == 0)
@@ -30,16 +30,25 @@ int main(int argc, char* argv[]) {
 	MPI_Barrier(MPI_COMM_WORLD);
 #  endif
 
-	/*-------------------------------------------ejecución del proceso principal--------------------------------------------*/
+	/*-------------------------------------------ejecuciÃ³n del proceso principal--------------------------------------------*/
 	int primos[100000];
 	vector<int> vPrime;
+	vector<pair<pair<int,int>,pair<int,int>>> resultados; 
+	vector<pair<pair<int,int>,pair<int,int>>> local_resultados; 
 	int j = 0, c = 0, n = 0;
-	if (mid == 0) {
+	if(mid==0){
 		cout << "Ingrese el rango superior(>5 && n divisible entre cantidad de procesos): " << endl;
 		cin >> n;
-		vPrime.reserve(n);
-		start = MPI_Wtime();
-		for (int i = 3; i <= n ; i+=2) {
+	}
+
+	/****Empeza a calcular WallTime****/
+	MPI_Barrier(MPI_COMM_WORLD);
+	local_start = MPI_Wtime();
+	/*********************************/
+
+	if (mid == 0) {
+		vPrime.push_back(2);
+		for (int i = 3; i < n ; i+=2) {
 			if (esPrimo(i)) {
 				vPrime.push_back(i);
 			}
@@ -56,14 +65,26 @@ int main(int argc, char* argv[]) {
 
 	MPI_Bcast(&vPrime[0], c, MPI_INT, 0, MPI_COMM_WORLD);
 	int local_n = (n-5) / cnt_proc;
-	int a, b, x, y, z; // para la suma
+	int a, b, s, x, y, z; // para la suma
 	
 	if (mid == 0) {
 		for (int i = 6; i < local_n+6; i++) {
-			a = b = c = 2;
-			x = y = z = 0;
+			//a = b = c = 2;
+			//x = y = z = 0;
 			if (i % 2 == 0) { // si es par
+				x=0;
+				y=vPrime.size();
+				a=2;
+				b=vPrime[y];
 				while (a + b != i) {
+					if((a+b)>i){
+						y--;
+						b=vPrime[y];
+					}else{
+						x++;
+						a=vPrime[x];
+					}
+					/*
 					if (x < vPrime.size()) {
 						a = vPrime[x];
 						x++;
@@ -80,10 +101,16 @@ int main(int argc, char* argv[]) {
 						x = 0; // Greeeeeeeeeeedy
 						a = 2;
 					}
+					*/
 				}
-				cout << i << " = " << a << " + " << b << " Hecho por proceso: " << mid << endl;
+				local_resultados.push_back(((i,a),(b,0)));
+				//cout << i << " = " << a << " + " << b << " Hecho por proceso: " << mid << endl;
 			}
 			else { // Si es impar
+				x=0;
+				y=vPrime.size();
+				a=2;
+				b=vPrime[y];
 				while (a + b + c != i) {
 					if (c < vPrime.size()) {
 						c = vPrime[z];
@@ -194,13 +221,18 @@ int main(int argc, char* argv[]) {
 			}
 		}
 	}
-	stop = MPI_Wtime();
-	cout << "Dura " << stop - start << endl;
-		/*-------------------------------------------finalización de la ejecución paralela---------------------------------------*/
+
+	local_finish = MPI_Wtime();
+	local_elapsed = local_finish âˆ’ local_start;
+	MPI_Reduce(&local_elapsed, &elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, comm);
+	if (mid == 0)
+		cout << "Tiempo transcurrido = " << elapsed << endl;
+
+		/*-------------------------------------------finalizaciÃ³n de la ejecuciÃ³n paralela---------------------------------------*/
 	if (mid == 0) {
 		cin.ignore();
 	}
-	MPI_Barrier(MPI_COMM_WORLD); // para sincronizar la finalización de los procesos
+	MPI_Barrier(MPI_COMM_WORLD); // para sincronizar la finalizaciÃ³n de los procesos
 
 	MPI_Finalize();
 	int chit = 0;
@@ -211,30 +243,30 @@ int main(int argc, char* argv[]) {
    /*---------------------------------------------------------------------
    * REQ: N/A
    * MOD: N/A
-   * EFE: despliega mensaje indicando cómo ejecutar el programa y pasarle parámetros de entrada.
+   * EFE: despliega mensaje indicando cÃ³mo ejecutar el programa y pasarle parÃ¡metros de entrada.
    * ENTRAN:
    *		nombre_prog:  nombre del programa
    * SALEN: N/A
    */
 void uso(string nombre_prog /* in */) {
-	cerr << nombre_prog.c_str() << " secuencia de parámetros de entrada" << endl;
+	cerr << nombre_prog.c_str() << " secuencia de parÃ¡metros de entrada" << endl;
 	exit(0);
 }  /* uso */
 
    /*---------------------------------------------------------------------
    * REQ: N/A
    * MOD: dato_salida
-   * EFE: obtiene los valores de los argumentos pasados por "línea de comandos".
+   * EFE: obtiene los valores de los argumentos pasados por "lÃ­nea de comandos".
    * ENTRAN:
    *		nombre_prog:  nombre del programa
    * SALEN:
-   *		dato_salida: un dato de salida con un valor de argumento pasado por "línea de comandos".
+   *		dato_salida: un dato de salida con un valor de argumento pasado por "lÃ­nea de comandos".
    */
 void obt_args(
 	char*    argv[]        /* in  */,
 	int&     dato_salida  /* out */) {
 
-	dato_salida = strtol(argv[1], NULL, 10); // se obtiene valor del argumento 1 pasado por "línea de comandos".
+	dato_salida = strtol(argv[1], NULL, 10); // se obtiene valor del argumento 1 pasado por "lÃ­nea de comandos".
 
 #  ifdef DEBUG
 	cout << "dato_salida = " << dato_salida << endl;
